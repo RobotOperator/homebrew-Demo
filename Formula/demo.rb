002bedabc6023762207e0ea7cb42ed4f6f5d5941
RobotOperator <79430515+RobotOperator@users.noreply.github.com>
2025-03-16 23:31:54 -0400

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -1,13 +1,13 @@
-class demo < Formula
+class Demo < Formula
   include Language::Python::Virtualenv
 
   desc "Demo presentation"
-  homepage "https://github.com/RobotOperator/Demo"
+  homepage "https://github.com/RobotOperator/homebrew-demo"
   url "http://192.168.0.65:8080/demo.tar.gz"
-  sha256 "e4bdc15d5b57eab9dba811529ec2e5631145ff1d2776facba4ca24c6094c394c"
+  sha256 "3202ee43b8f981986b09900d2dd264e7a8cfbfd0b23e9bb00afe151387072407"
   version "1.31.1"
 
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   resource "pyobjc-framework-OSAKit" do
     url "https://files.pythonhosted.org/packages/8a/ab/5df750cc0a30c24233ad37608ad896a2759ef65ec80eebcd10e52dcdbf36/pyobjc_framework_osakit-10.3.tar.gz"
